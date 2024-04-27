271210119bdb049df213ec259584c268b2b495ad
Vladislav Makhobey <57014028+shibazzaki@users.noreply.github.com>
2024-04-27 16:07:54 +0300

    v 1.0.4 добавив шифрування для токенак, та виніс це все в окремий файо

--- bot.py
+++ bot.py
@@ -1,14 +1,20 @@
 import asyncio
 import logging
+import colorama
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
-
+from datetime import datetime
+from config_reader import config
 
 
 logging.basicConfig(level=logging.INFO)
-bot = Bot(token="7037127414:AAFPTG7YZjquoYiqY6gTv1Ez2krZquEjelU") #token would be here
-
+bot = Bot(token=config.bot_token.get_secret_value())
 dp = Dispatcher()
+
+dp["started_at"] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+async def on_startup(dp: Dispatcher):
+    await dp.start_polling(bot, mylist=[1, 2, 3])
 
 
 #handler command start
@@ -17,10 +23,25 @@ async def start(message: types.Message):
     await message.answer("Hello!!!HI!!!! my sweat Stepovyata")
 
 
+@dp.message(Command("help"))
+async def cmd_reply(message: types.Message):
+    await message.reply("What do u need?")
+
+@dp.message(Command("dice"))
+async def cmd_dice(message: types.Message):
+    await message.answer_dice(emoji="🎲")
+
+
+@dp.message(Command("info"))
+async def cmd_info(message: types.Message, started_at: str):
+    await message.answer(f"Бот запущений {started_at}")
+
 async def main():
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
     asyncio.run(main())
+
+
 
 
