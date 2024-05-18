a049bb2aa6fafa4a64434b92de49c743b055c29f
shiba <failikyt400@gmail.com>
2024-05-18 16:55:45 +0300

    butttonnnn

--- bot.py
+++ bot.py
@@ -22,9 +22,26 @@ async def on_startup(dp: Dispatcher):
 
 #handler command start
 @dp.message(Command("start"))
-async def start(message: types.Message):
-    await message.answer("Hello!!!HI!!!! my sweat Stepovyata")
+async def cmd_start(message: types.Message):
+    kb = [
+        [types.KeyboardButton(text='джедай')],
+        [types.KeyboardButton(text='ситх')]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        one_time_keyboard=True,
+        input_field_placeholder="що обереш в листопаді"
+    )
+    await message.answer("листопад", reply_markup=keyboard)
 
+@dp.message(F.text.lower() == "джедай")
+async def djedai(message: types.Message):
+    await message.reply("Трушно")
+
+@dp.message(F.text.lower() == "ситх")
+async def sitkh(message: types.Message):
+    await message.reply(f"Ти обіцяв боротися зі злом, а не примкнути до нього")
 
 @dp.message(Command("help"))
 async def cmd_reply(message: types.Message):
